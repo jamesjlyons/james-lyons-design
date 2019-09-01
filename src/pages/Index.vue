c50b0210e0221bf4f61155f8b7ff216684944679
James Lyons <jamesjlyons29@gmail.com>
2019-08-31 22:19:56 -0700

    intro frame layout css grid
    add links behind frame with css grid, add media queries

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,51 +9,42 @@
             </div>
           </div>
           <div class="intro-links">
-
+            <a href="https://dribbble.com/jamesjlyons" target="_blank" class="button-link">
+              <span>🏀</span>
+            </a>
+            <a href="https://github.com/jamesjlyons" target="_blank" class="button-link">
+              <span>👨‍💻</span>
+            </a>
+            <a href="mailto:jamesjlyons29@gmail.com" target="_blank" class="button-link">
+              <span>✉️</span>
+            </a>
           </div>
         </div>
-        <div class="intro-type">
-          <h5 class="small-margin">Hello, I'm</h5>
-          <h2 class="small-margin">James Lyons</h2>
-          <h4 class="small-margin">
-            Product Designer at
-            <a href="https://liferay.design">Liferay</a>.
-          </h4>
-          <h4 class="small-margin">
-            Making things with
-            <a href="http://tapfive.io/">Tap&nbsp;Five</a>.
-          </h4>
-          <h5 class="small-margin previous">
-            Previously Lead Designer at
-            <a href="https://www.grandpad.net">GrandPad</a>.
-          </h5>
-        </div>
+        <transition name="fade" appear>
+          <div class="intro-type">
+            <h5 class="small-margin">Hello, I'm</h5>
+            <h2 class="small-margin">James Lyons</h2>
+            <h4 class="small-margin">
+              Product Designer at
+              <a
+                href="https://liferay.design"
+                target="_blank"
+                v-on:mouseenter="liferay"
+                v-on:mouseleave="wave"
+              >Liferay</a>.
+            </h4>
+            <h4 class="small-margin">
+              Making things with
+              <a href="http://tapfive.io/" target="_blank">Tap&nbsp;Five</a>.
+            </h4>
+            <h5 class="small-margin previous">
+              Previously Lead Designer at
+              <a href="https://www.grandpad.net" target="_blank">GrandPad</a>.
+            </h5>
+          </div>
+        </transition>
       </div>
     </section>
-    <!-- <div class="links">
-        <h5>
-          <a href="mailto:jamesjlyons29@gmail.com" target="_blank" class="button-link">
-            <span>✉️</span>&nbsp;Email
-          </a>
-        </h5>
-        <h5>
-          <a href="https://dribbble.com/jamesjlyons" target="_blank" class="button-link">
-            <span>🏀</span>&nbsp;Dribbble
-          </a>
-        </h5>
-        <h5>
-          <a href="https://github.com/jamesjlyons" target="_blank" class="button-link">
-            <span>👨‍💻</span>&nbsp;GitHub
-          </a>
-        </h5>
-    </div>-->
-    <!-- <br />
-      <br />
-      <br />
-    </div>
-    <div class="werk text_small">
-      <g-image alt="party parrot construction" src="~/assets/fixparrot.gif" width="16" />Currently getting werk done on this site.
-    </div>-->
   </Layout>
 </template>
 
@@ -61,17 +52,42 @@
 export default {
   metaInfo: {
     title: ""
+  },
+  methods: {
+    wave: function(event) {
+      console.log("wave");
+    },
+    liferay: function(event) {
+      // const frame = document.getElementsByClassName('intro-frame');
+      // document.getElementsByClassName('intro-frame').classList.add('liferay');
+      console.log("liferay");
+    }
   }
 };
 </script>
 
 <style lang="postcss">
+.fade-enter-active {
+  transition: opacity 1s;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+
 span.wave {
   animation-name: wave-animation;
   animation-duration: 2.5s;
   animation-iteration-count: 1;
   transform-origin: 70% 70%;
   display: inline-block;
+  cursor: grab;
+}
+
+span.wave:hover {
+  animation-name: wave-animation;
+  animation-duration: 1.5s;
+  animation-iteration-count: infinite;
 }
 @keyframes wave-animation {
   0% {
@@ -108,6 +124,7 @@ span.wave {
 
 .intro-hero {
   display: flex;
+  flex-direction: row;
 }
 
 .intro-graphic {
@@ -116,11 +133,17 @@ span.wave {
   border-radius: 16px;
   height: 208px;
   width: 196px; */
+  display: grid;
+  grid-template-columns: 196px;
+  grid-template-rows: 140px repeat(2, 56px);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px; 
 }
 
 .intro-frame {
-  width: 196px;
-  height: 196px;
+  grid-area: 1 / 1 / 3 / 2;
+  /* width: 196px;
+  height: 196px; */
   background: rgb(255, 255, 255);
   border: 4px solid rgb(235, 235, 235);
   box-shadow: 0px 12px 24px -9px rgba(0, 0, 0, 0.5);
@@ -130,13 +153,29 @@ span.wave {
   justify-content: center;
 }
 
+.intro-frame.liferay {
+  background: blue;
+}
+
 .frame-wave {
   font-size: 72px;
   transform: translateX(-5%);
 }
 
 .intro-links {
-  
+  grid-area:  2 / 1 / 4 / 2;
+  width: 196px;
+  padding-bottom: 14px;
+  z-index: -1;
+  background: rgb(243, 243, 243);
+  border-radius: 0 0 16px 16px;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  align-items: flex-end;
+  & a {
+    text-decoration: none;
+  }
 }
 
 .intro-type {
@@ -145,10 +184,10 @@ span.wave {
     text-decoration: underline var(--medium);
     text-decoration-skip-ink: auto;
     transition-property: text-decoration;
-    transition-duration: .3s;
+    transition-duration: 0.3s;
     transition-timing-function: ease;
   }
-   & a:hover {
+  & a:hover {
     color: var(--dark);
     text-decoration: underline var(--green);
   }
@@ -163,5 +202,30 @@ span.wave {
   & a {
     color: var(--medium);
   }
+}
+
+/* media queries */
+
+/* Small (sm) */
+@media (min-width: 640px) {
+  /* ... */
+}
+
+/* Medium (md) */
+@media (max-width: 768px) {
+  .intro-hero {
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
+/* Large (lg) */
+@media (min-width: 1024px) {
+  /* ... */
+}
+
+/* Extra Large (xl) */
+@media (min-width: 1280px) {
+  /* ... */
 }
 </style>
