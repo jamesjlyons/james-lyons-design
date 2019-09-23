137b0a8aaf2b61affe1359553ae4cc3112900b31
James Lyons <jamesjlyons29@gmail.com>
2019-09-22 22:52:50 -0700

    Project layout progress

--- src/templates/Project.vue
+++ src/templates/Project.vue
@@ -1,8 +1,13 @@
 <template>
   <Layout>
-    <h1 v-if="$page.project">{{ $page.project.title }}</h1>
-    <div v-if="$page.project" v-html="$page.project.content"></div>
-    <g-image v-if="$page.project" :src="$page.project.thumbnail" />
+    <section class="project-intro">
+      <h2 v-if="$page.project">{{ $page.project.title }}</h2>
+      <h4 v-if="$page.project">{{ $page.project.description }}</h4>
+      <!-- <g-image v-if="$page.project" :src="$page.project.thumbnail" /> -->
+    </section>
+    <section class="project-content">
+      <div v-if="$page.project" v-html="$page.project.content"></div>
+    </section>
   </Layout>
 </template>
 
@@ -23,4 +28,28 @@ export default {};
 </script>
 
 <style lang="postcss">
+.project-intro {
+  max-width: 980px;
+  margin: auto;
+  min-height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.project-content {
+  max-width: 980px;
+  margin: auto;
+  & p {
+    max-width: 80ch;
+  }
+  & ul {
+    max-width: 80ch;
+  }
+
+  & iframe {
+    box-shadow: 0px 12px 24px -9px rgba(0, 0, 0, 0.5);
+  }
+
+}
 </style>
