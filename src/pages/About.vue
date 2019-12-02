a9998ee9838a2dfbb6def4afa2288a7641bef2ed
James Lyons <jamesjlyons29@gmail.com>
2019-12-01 18:22:07 -0800

    add about me page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,8 +1,14 @@
 <template>
   <Layout>
     <section>
-      <h1>About me</h1>
-      <p>Coming later.</p>
+      <h2>About me</h2>
+      <p>I am a Product Designer at Liferay in Southern California. I'm currently designing on the Analytics Cloud product team, and made a handy Figma plugin to help our process.
+        <br /> <br />
+      I also currently design and code stuff like Subdivide and Lio with Tap Five, a team of two who likes to make things that don't suck.
+        <br /> <br />
+      I previously worked as the Lead Designer at GrandPad. I was the first designer the startup had hired in it's three years of existence, and I did much of the front-end development for the main website and checkout. During my time there, I established a UX design process within the company by encouraging user research, idea validation, and interactive prototype testing. I helped raise the quality of product designs by establishing cohesive visual patterns across websites and mobile apps. I led the process of two different website redesigns, launched iOS and Android app redesigns, set up A/B tests, designed and coded emails, and did some minor branding work.</p>
+      <br /> <br />
+      
     </section>
   </Layout>
 </template>
@@ -10,12 +16,12 @@
 <script>
 export default {
   metaInfo: {
-    title: "About"
+    title: "About Me"
   }
 };
 </script>
 
-<style lang="postcss" scoped>
+<style lang="postcss">
 section {
   max-width: 980px;
   margin: auto;
@@ -23,6 +29,5 @@ section {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  color: var(--medium);
 }
 </style>
