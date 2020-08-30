c67f1377b656c0357422647fdd17bf0c157404ef
James Lyons <jamesjlyons29@gmail.com>
2020-08-29 19:13:10 -0700

    feat: Blog list styling

--- pages/some-thoughts.vue
+++ pages/some-thoughts.vue
@@ -5,13 +5,17 @@
       <h6>Some thoughts about design and life</h6>
     </div>
     <div class="thought-index">
-      <div v-for="post in blog" :key="post.slug" v-bind="post">
-        <h5>{{ post.title }}</h5>
-        <h6>{{ new Date(post.createdAt).toDateString() }}</h6>
-        <p>{{ post.description }}</p>
+      <div
+        v-for="post in blog"
+        :key="post.slug"
+        v-bind="post"
+        class="thought-li"
+      >
         <nuxt-link :to="`/thoughts/${post.slug}`" class="button"
-          >Read
+          ><h5>{{ post.title }}</h5>
         </nuxt-link>
+        <h6>{{ post.subtitle }}</h6>
+        <p class="date">{{ new Date(post.createdAt).toDateString() }}</p>
       </div>
       <div class="empty-state">
         <h4>Doesn't look like anything to me.</h4>
@@ -67,6 +71,35 @@ export default {
   justify-content: center;
 }
 
+.thought-li {
+  padding: 16px 16px 16px 24px;
+  margin-bottom: 48px;
+  border-left: 1px solid var(--border);
+  & h5 {
+    margin-top: 0;
+  }
+  & h6 {
+    color: var(--font-p);
+  }
+  & .date {
+    color: var(--medium);
+    margin-bottom: 0;
+  }
+
+  & a {
+    color: var(--font-main);
+    font-weight: 600;
+    text-decoration: underline var(--medium);
+    text-decoration-skip-ink: auto;
+    transition-property: text-decoration, color;
+    transition-duration: 0.3s;
+    transition-timing-function: ease-in-out;
+  }
+  & a:hover {
+    color: var(--font-main);
+    text-decoration: underline var(--green);
+  }
+}
 .empty-state {
   opacity: 0.3;
 }
