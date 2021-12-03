74184282e7083879ae0bbabf8b0619867e6166b9
James Lyons <jamesjlyons29@gmail.com>
2021-12-02 22:33:24 -0800

    Add noreferrer for security

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,36 +44,48 @@ export default function Home() {
         Product Designer & Frontend Developer
       </Text>
       <Text size="1">
-        I‘m currently a Sr. Product Designer and Front-End Developer at{" "}
-        <a href="https://soberlink.com" target="_blank">
+        I&apos;m currently a Sr. Product Designer and Front-End Developer at{" "}
+        <a href="https://soberlink.com" target="_blank" rel="noreferrer">
           Soberlink
         </a>
         . I also currently design and code stuff with{" "}
-        <a href="https://tapfive.dev" target="_blank">
+        <a href="https://tapfive.dev" target="_blank" rel="noreferrer">
           Tap Five
         </a>
-        , a tiny team who like to make things that don't suck. <br />
+        , a tiny team who like to make things that don&apos;t suck. <br />
         <br />
         Before this was a Product Designer at{" "}
-        <a href="https://liferay.com" target="_blank">
+        <a href="https://liferay.com" target="_blank" rel="noreferrer">
           Liferay
         </a>{" "}
         on the Analytics Cloud product team, and previously worked as the Lead
         Designer at{" "}
-        <a href="https://grandpad.net" target="_blank">
+        <a href="https://grandpad.net" target="_blank" rel="noreferrer">
           GrandPad
         </a>
         .
         <br /> <br /> You can find me on{" "}
-        <a href="https://dribbble.com/jamesjlyons" target="_blank">
+        <a
+          href="https://dribbble.com/jamesjlyons"
+          target="_blank"
+          rel="noreferrer"
+        >
           Dribbble
         </a>
         , and{" "}
-        <a href="https://github.com/jamesjlyons" target="_blank">
+        <a
+          href="https://github.com/jamesjlyons"
+          target="_blank"
+          rel="noreferrer"
+        >
           GitHub
         </a>
         , or you can reach out and{" "}
-        <a href="mailto:hello@jameslyons.design" target="_blank">
+        <a
+          href="mailto:hello@jameslyons.design"
+          target="_blank"
+          rel="noreferrer"
+        >
           say hello
         </a>
         .
