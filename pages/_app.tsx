e0bc60b5d57dddf0bf66e8f6444ee6de6b43013d
James Lyons <jamesjlyons29@gmail.com>
2021-12-08 22:08:23 -0800

    Import globalCss from Stitches config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 // import '../styles/globals.css'
-import { globalCss } from "@stitches/react";
+import { globalCss } from "../stitches.config";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import Head from "next/head";
