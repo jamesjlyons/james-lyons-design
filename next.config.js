e905f4e87ed43b0ba941d30f13b22665e9e881ba
James Lyons <jamesjlyons29@gmail.com>
2021-12-15 19:47:08 -0800

    add @next/mdx
    routes are broken

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+};
+const withMDX = require("@next/mdx")({
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+module.exports = withMDX();
