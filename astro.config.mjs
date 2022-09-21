7cadd3251dc0e589ce3dc1eccdb4d079069716ca
James Lyons <hello@jameslyons.design>
2022-09-20 18:55:00 -0700

    Upgrade astro and install tokencss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import tokencss from "@tokencss/astro";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tokencss()]
+});
